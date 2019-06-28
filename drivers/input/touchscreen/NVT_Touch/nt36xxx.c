0907f8b18e53cf9a7e4c51031d0bbea9c6d61411
Panchajanya1999 <rsk52959@gmail.com>
2019-06-28 11:24:13 +0530

    nt36xxx: Avoid allocating wq to unbound CPUs
    This actually solves screen stutters which occurs during CPU intensive tasks or games.
    
    Reference: 51c810546f4bb50065c32d27588cd730b3533a65
    
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- drivers/input/touchscreen/NVT_Touch/nt36xxx.c
+++ drivers/input/touchscreen/NVT_Touch/nt36xxx.c
@@ -1630,7 +1630,7 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 	mutex_unlock(&ts->lock);
 
 	//---allocate workqueue---
-	nvt_wq = alloc_workqueue("nvt_wq", WQ_HIGHPRI | WQ_UNBOUND, 0);
+	nvt_wq = alloc_workqueue("nvt_wq", WQ_HIGHPRI, 0);
 	if (!nvt_wq) {
 		NVT_ERR("nvt_wq create workqueue failed\n");
 		ret = -ENOMEM;
